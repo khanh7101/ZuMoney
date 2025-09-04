b45435ab5b4b30a493dab5a3e9e6dd27acc47f9e
DESKTOP-IBHT8HS\khanh <khanhvuduy@gmail.com>
2025-09-04 16:18:02 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles.css'
-import { registerSW } from 'vite-plugin-pwa/client'
+import { registerSW } from 'virtual:pwa-register'
 registerSW()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
